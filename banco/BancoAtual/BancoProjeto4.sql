eb9fbfe35ba6fc79ddeeea60dbf550ca5ad754f1
juniorHarper <junior_junior_17@hotmail.com>
2014-04-03 21:22:12 -0300

    BancoAtual

---
+++ banco/BancoAtual/BancoProjeto4.sql
@@ -0,0 +1,81 @@
+CREATE TABLE Relatorio (
+IdRelatorio Int PRIMARY KEY,
+ValorTotal money,
+Data nvarchar(20),
+CodServ nvarchar(20),
+CodProd nvarchar(20),
+FuncCPF nvarchar(20),
+ClienCPF nvarchar(20)
+)
+
+CREATE TABLE Usuario (
+IdUsuario Int PRIMARY KEY,
+TipoUsar Int,
+Login nvarchar(20),
+Senha nvarchar(20),
+Nome nvarchar(20)
+)
+
+CREATE TABLE Serviços (
+CodServ nvarchar(20) PRIMARY KEY,
+Descrição nvarchar(20),
+Valor money
+)
+
+CREATE TABLE Funcionario (
+CPF nvarchar(20) PRIMARY KEY,
+Nome nvarchar(20),
+Telefone nvarchar(20),
+Endereco nvarchar(20),
+Porcentagem Int,
+DatNascimento nvarchar(20)
+)
+
+CREATE TABLE ManterFuncionario (
+FuncCPF nvarchar(20),
+IdUsuarioF Int,
+FOREIGN KEY(FuncCPF) REFERENCES Funcionario (CPF),
+FOREIGN KEY(IdUsuarioF) REFERENCES Usuario (IdUsuario)
+)
+
+CREATE TABLE Cliente (
+CPF nvarchar(20) PRIMARY KEY,
+Endereço nvarchar(20),
+Telefone nvarchar(20),
+Telefone2 nvarchar(20),
+DatNascimento nvarchar(20),
+Email nvarchar(20)
+)
+
+CREATE TABLE ManterUsuario (
+IdUsuario Int,
+ClienCPF nvarchar(20),
+FOREIGN KEY(IdUsuario) REFERENCES Usuario (IdUsuario),
+FOREIGN KEY(ClienCPF) REFERENCES Cliente (CPF)
+)
+
+CREATE TABLE Produtos (
+CodProd nvarchar(20) PRIMARY KEY,
+Descrição nvarchar(20),
+Valor Money,
+QntProd Int
+)
+
+CREATE TABLE ManterProdutos (
+CodProdt nvarchar(20),
+IdUsuarioP Int,
+FOREIGN KEY(CodProdt) REFERENCES Produtos (CodProd),
+FOREIGN KEY(IdUsuarioP) REFERENCES Usuario (IdUsuario)
+)
+
+CREATE TABLE ManterServico (
+CodServc nvarchar(20),
+IdUsuarioS Int,
+FOREIGN KEY(CodServc) REFERENCES Serviços (CodServ),
+FOREIGN KEY(IdUsuarioS) REFERENCES Usuario (IdUsuario)
+)
+
+ALTER TABLE Relatorio ADD FOREIGN KEY(CodServc) REFERENCES Serviços (CodServ)
+ALTER TABLE Relatorio ADD FOREIGN KEY(CodProdt) REFERENCES Produtos (CodProd)
+ALTER TABLE Relatorio ADD FOREIGN KEY(FuncCPF) REFERENCES Funcionario (CPF)
+ALTER TABLE Relatorio ADD FOREIGN KEY(ClienCPF) REFERENCES Cliente (CPF)
