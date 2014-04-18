79d032c68ef94f97925cd7a864f1c0e94589dc0c
VictorCelso <victorcelsoct@hotmail.com>
2014-04-17 23:07:09 -0300

    Atualização do script do banco

--- banco/BancoAtual/BancoProjeto4.sql
+++ banco/BancoAtual/BancoProjeto4.sql
@@ -1,87 +1,57 @@
-CREATE TABLE Relatorio (
-IdRelatorio Int PRIMARY KEY,
-ValorTotal money,
-Data nvarchar(100),
-CodServ nvarchar(100),
-CodProd nvarchar(100),
-FuncCPF nvarchar(100),
-ClienCPF nvarchar(100)
-)
-
 CREATE TABLE Usuario (
-IdUsuario Int PRIMARY KEY,
-isGestor Int,
-Login nvarchar(100),
+IdUsuario Int PRIMARY KEY identity,
+tipoUsuario Int,
+Login nvarchar(100) unique,
 Senha nvarchar(100),
-Nome nvarchar(100),
-Excluido bit
-)
-
-CREATE TABLE Servicos (
-IdServicos Int PRIMARY KEY,
-Descricao nvarchar(100),
-Valor money
-)
-
-CREATE TABLE Funcionario (
-CPF nvarchar(100) PRIMARY KEY,
-Nome nvarchar(100),
-Telefone nvarchar(100),
-Endereco nvarchar(100),
-Porcentagem Double,
-Data_Nascimento nvarchar(100),
-Excluido bit
-)
-
-CREATE TABLE ManterFuncionario (
-FuncCPF nvarchar(100),
-IdUsuarioF Int,
-FOREIGN KEY(FuncCPF) REFERENCES Funcionario (CPF),
-FOREIGN KEY(IdUsuarioF) REFERENCES Usuario (IdUsuario)
+Nome nvarchar(20),
+Ativo bit
 )
 
 CREATE TABLE Cliente (
-CPF nvarchar(100) PRIMARY KEY,
-Endereco nvarchar(100),
-Telefone nvarchar(100),
-Telefone2 nvarchar(100),
-Data_Nascimento nvarchar(100),
-Email nvarchar(100),
+CPF nvarchar(20) PRIMARY KEY,
+Endereço nvarchar(200),
+Telefone nvarchar(30),
+DataNascimento nvarchar(20),
+Email nvarchar(20)
+Ativo bit
+)
+
+CREATE TABLE Funcionario (
+CPF nvarchar(20) PRIMARY KEY,
 Nome nvarchar(100),
-Excluido bit
+Telefone nvarchar(30),
+Endereco nvarchar(200),
+Porcentagem float,
+DataNascimento nvarchar(20)
+Ativo bit
 )
 
-CREATE TABLE ManterUsuario (
-IdUsuario Int,
-ClienCPF nvarchar(100),
-FOREIGN KEY(IdUsuario) REFERENCES Usuario (IdUsuario),
-FOREIGN KEY(ClienCPF) REFERENCES Cliente (CPF)
-)
 
-CREATE TABLE Produtos (
-IdProduto Int PRIMARY KEY,
-Codigo varchar(100),
+CREATE TABLE Serviços (
+IdServico int PRIMARY KEY identity,
 Descricao nvarchar(100),
+Valor money
+Ativo bit
+)
+
+CREATE TABLE Produto (
+IdProduto int PRIMARY KEY identity,
+Descricao nvarchar(20),
 Valor Money,
-Quantidade_Estoque Int,
-Excluido bit
+Quantidade Int
+Ativo bit
 )
 
-CREATE TABLE ManterProdutos (
-CodProdt nvarchar(100),
-IdUsuarioP Int,
-FOREIGN KEY(CodProdt) REFERENCES Produtos (CodProd),
-FOREIGN KEY(IdUsuarioP) REFERENCES Usuario (IdUsuario)
+CREATE TABLE Venda (
+IdVenda Int PRIMARY KEY identity,
+ValorTotal money,
+Data nvarchar(20),
+IdServico int,
+IdProduto int,
+CPFFuncionario nvarchar(20),
+CPFCliente nvarchar(20),
+FOREIGN KEY(IdServico) REFERENCES Serviço (IdServico),
+FOREIGN KEY(IdProduto) REFERENCES Produtos (IdProduto),
+FOREIGN KEY(CPFFuncionario) REFERENCES Funcionario (CPF),
+FOREIGN KEY(CPFCliente) REFERENCES Cliente (CPF)
 )
-
-CREATE TABLE ManterServico (
-CodServc nvarchar(100),
-IdUsuarioS Int,
-FOREIGN KEY(CodServc) REFERENCES Servicos (CodServ),
-FOREIGN KEY(IdUsuarioS) REFERENCES Usuario (IdUsuario)
-)
-
-ALTER TABLE Relatorio ADD FOREIGN KEY(CodServc) REFERENCES Servicos (CodServ)
-ALTER TABLE Relatorio ADD FOREIGN KEY(CodProdt) REFERENCES Produtos (CodProd)
-ALTER TABLE Relatorio ADD FOREIGN KEY(FuncCPF) REFERENCES Funcionario (CPF)
-ALTER TABLE Relatorio ADD FOREIGN KEY(ClienCPF) REFERENCES Cliente (CPF)
