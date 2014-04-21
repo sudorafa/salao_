9837ef9cd72859479f648caa5919817035ccb2ce
juniorHarper <junior_junior_17@hotmail.com>
2014-04-21 01:19:53 -0300

    Detalhar usuario funcionando!

---
+++ Projeto/Projeto4_Junior/InterfaceGrafica/Usuario/DetalharUsuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto4_Junior.InterfaceGrafica.Usuario
+{
+    public partial class DetalharUsuario : Form
+    {
+        private Modelo.Usuario usu;
+
+        public DetalharUsuario()
+        {
+            InitializeComponent();
+        }
+
+        public DetalharUsuario(Modelo.Usuario usu)
+        {
+            InitializeComponent();
+            NomeUsu.Text = usu.Nome;
+            LoginUsu.Text = usu.Login;
+            SenhaUsu.Text = usu.Senha;
+            if (usu.isGestor == 1)
+            {
+                TipoUsu.Text = "Gestor";
+            }
+            else
+            {
+                TipoUsu.Text = "Usuario";
+            }
+
+            this.usu = usu;
+        }
+    }
+}
