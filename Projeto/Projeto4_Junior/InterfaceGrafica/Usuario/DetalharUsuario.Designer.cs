d229ea9f1e3eafde4e63edea1b0fa7c4384ec8c4
juniorHarper <junior_junior_17@hotmail.com>
2014-04-25 12:16:43 -0300

    Detalhar usuario funcionando!

--- Projeto/Projeto4_Junior/InterfaceGrafica/Usuario/DetalharUsuario.Designer.cs
+++ Projeto/Projeto4_Junior/InterfaceGrafica/Usuario/DetalharUsuario.Designer.cs
@@ -28,80 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.TipoUsu = new System.Windows.Forms.TextBox();
-            this.LoginUsu = new System.Windows.Forms.TextBox();
-            this.SenhaUsu = new System.Windows.Forms.TextBox();
-            this.NomeUsu = new System.Windows.Forms.TextBox();
+            this.DadosUsuario = new System.Windows.Forms.GroupBox();
+            this.Senha_Usu = new System.Windows.Forms.TextBox();
+            this.Login_Usu = new System.Windows.Forms.TextBox();
+            this.Nome_Usu = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Tipo_Usu = new System.Windows.Forms.TextBox();
+            this.DadosUsuario.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // DadosUsuario
             // 
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.TipoUsu);
-            this.groupBox1.Controls.Add(this.LoginUsu);
-            this.groupBox1.Controls.Add(this.SenhaUsu);
-            this.groupBox1.Controls.Add(this.NomeUsu);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(334, 193);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Usuario";
+            this.DadosUsuario.Controls.Add(this.Tipo_Usu);
+            this.DadosUsuario.Controls.Add(this.label4);
+            this.DadosUsuario.Controls.Add(this.Senha_Usu);
+            this.DadosUsuario.Controls.Add(this.Login_Usu);
+            this.DadosUsuario.Controls.Add(this.Nome_Usu);
+            this.DadosUsuario.Controls.Add(this.label3);
+            this.DadosUsuario.Controls.Add(this.label2);
+            this.DadosUsuario.Controls.Add(this.label1);
+            this.DadosUsuario.Location = new System.Drawing.Point(12, 12);
+            this.DadosUsuario.Name = "DadosUsuario";
+            this.DadosUsuario.Size = new System.Drawing.Size(324, 169);
+            this.DadosUsuario.TabIndex = 1;
+            this.DadosUsuario.TabStop = false;
+            this.DadosUsuario.Text = "Usuario";
             // 
-            // label4
+            // Senha_Usu
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(40, 152);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Tipo:";
+            this.Senha_Usu.Location = new System.Drawing.Point(50, 94);
+            this.Senha_Usu.Name = "Senha_Usu";
+            this.Senha_Usu.ReadOnly = true;
+            this.Senha_Usu.Size = new System.Drawing.Size(134, 20);
+            this.Senha_Usu.TabIndex = 5;
             // 
-            // TipoUsu
+            // Login_Usu
             // 
-            this.TipoUsu.Location = new System.Drawing.Point(84, 149);
-            this.TipoUsu.Name = "TipoUsu";
-            this.TipoUsu.ReadOnly = true;
-            this.TipoUsu.Size = new System.Drawing.Size(104, 20);
-            this.TipoUsu.TabIndex = 6;
+            this.Login_Usu.Location = new System.Drawing.Point(50, 64);
+            this.Login_Usu.Name = "Login_Usu";
+            this.Login_Usu.ReadOnly = true;
+            this.Login_Usu.Size = new System.Drawing.Size(134, 20);
+            this.Login_Usu.TabIndex = 4;
             // 
-            // LoginUsu
+            // Nome_Usu
             // 
-            this.LoginUsu.Location = new System.Drawing.Point(84, 71);
-            this.LoginUsu.Name = "LoginUsu";
-            this.LoginUsu.ReadOnly = true;
-            this.LoginUsu.Size = new System.Drawing.Size(104, 20);
-            this.LoginUsu.TabIndex = 5;
-            // 
-            // SenhaUsu
-            // 
-            this.SenhaUsu.Location = new System.Drawing.Point(84, 106);
-            this.SenhaUsu.Name = "SenhaUsu";
-            this.SenhaUsu.ReadOnly = true;
-            this.SenhaUsu.Size = new System.Drawing.Size(104, 20);
-            this.SenhaUsu.TabIndex = 4;
-            // 
-            // NomeUsu
-            // 
-            this.NomeUsu.Location = new System.Drawing.Point(84, 33);
-            this.NomeUsu.Name = "NomeUsu";
-            this.NomeUsu.ReadOnly = true;
-            this.NomeUsu.Size = new System.Drawing.Size(228, 20);
-            this.NomeUsu.TabIndex = 3;
+            this.Nome_Usu.Location = new System.Drawing.Point(50, 33);
+            this.Nome_Usu.Name = "Nome_Usu";
+            this.Nome_Usu.ReadOnly = true;
+            this.Nome_Usu.Size = new System.Drawing.Size(237, 20);
+            this.Nome_Usu.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(40, 113);
+            this.label3.Location = new System.Drawing.Point(6, 97);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 13);
             this.label3.TabIndex = 2;
@@ -110,7 +93,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(40, 74);
+            this.label2.Location = new System.Drawing.Point(6, 64);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(36, 13);
             this.label2.TabIndex = 1;
@@ -119,35 +102,52 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 36);
+            this.label1.Location = new System.Drawing.Point(6, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome:";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tipo Usuario:";
+            // 
+            // Tipo_Usu
+            // 
+            this.Tipo_Usu.Location = new System.Drawing.Point(82, 127);
+            this.Tipo_Usu.Name = "Tipo_Usu";
+            this.Tipo_Usu.ReadOnly = true;
+            this.Tipo_Usu.Size = new System.Drawing.Size(100, 20);
+            this.Tipo_Usu.TabIndex = 7;
+            // 
             // DetalharUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(357, 216);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(349, 201);
+            this.Controls.Add(this.DadosUsuario);
             this.Name = "DetalharUsuario";
             this.Text = "DetalharUsuario";
             this.Load += new System.EventHandler(this.DetalharUsuario_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.DadosUsuario.ResumeLayout(false);
+            this.DadosUsuario.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox DadosUsuario;
+        private System.Windows.Forms.TextBox Tipo_Usu;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox TipoUsu;
-        private System.Windows.Forms.TextBox LoginUsu;
-        private System.Windows.Forms.TextBox SenhaUsu;
-        private System.Windows.Forms.TextBox NomeUsu;
+        private System.Windows.Forms.TextBox Senha_Usu;
+        private System.Windows.Forms.TextBox Login_Usu;
+        private System.Windows.Forms.TextBox Nome_Usu;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
