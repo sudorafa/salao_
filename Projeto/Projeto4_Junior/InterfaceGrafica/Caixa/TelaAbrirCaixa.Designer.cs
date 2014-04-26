5bd911daef396c793f7a33f533bead096ec79777
Paulo Correia Filho <paulinho_pe158@hotmail.com>
2014-04-26 00:14:34 -0300

    Implemetando o layout da tela de caixa, em andamento

--- Projeto/Projeto4_Junior/InterfaceGrafica/Caixa/TelaAbrirCaixa.Designer.cs
+++ Projeto/Projeto4_Junior/InterfaceGrafica/Caixa/TelaAbrirCaixa.Designer.cs
@@ -30,24 +30,38 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbCpfCliente = new System.Windows.Forms.TextBox();
+            this.tbNomeCliente = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btBuscarCliente = new System.Windows.Forms.Button();
             this.tbBuscarCpfCliente = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.cbFuncionario = new System.Windows.Forms.ComboBox();
-            this.btBuscarCliente = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tbNomeCliente = new System.Windows.Forms.TextBox();
-            this.tbCpfCliente = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.cbServicos = new System.Windows.Forms.ComboBox();
+            this.cbProdutos = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.remover = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.lbTotal = new System.Windows.Forms.Label();
+            this.btFinalizarVenda = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(116, 9);
+            this.label1.Location = new System.Drawing.Point(261, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(173, 55);
             this.label1.TabIndex = 0;
@@ -69,6 +83,49 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Cliente";
             // 
+            // tbCpfCliente
+            // 
+            this.tbCpfCliente.Location = new System.Drawing.Point(65, 61);
+            this.tbCpfCliente.Name = "tbCpfCliente";
+            this.tbCpfCliente.ReadOnly = true;
+            this.tbCpfCliente.Size = new System.Drawing.Size(133, 20);
+            this.tbCpfCliente.TabIndex = 5;
+            // 
+            // tbNomeCliente
+            // 
+            this.tbNomeCliente.Location = new System.Drawing.Point(65, 36);
+            this.tbNomeCliente.Name = "tbNomeCliente";
+            this.tbNomeCliente.ReadOnly = true;
+            this.tbNomeCliente.Size = new System.Drawing.Size(225, 20);
+            this.tbNomeCliente.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CPF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nome:";
+            // 
+            // btBuscarCliente
+            // 
+            this.btBuscarCliente.Location = new System.Drawing.Point(215, 11);
+            this.btBuscarCliente.Name = "btBuscarCliente";
+            this.btBuscarCliente.Size = new System.Drawing.Size(75, 23);
+            this.btBuscarCliente.TabIndex = 2;
+            this.btBuscarCliente.Text = "Buscar";
+            this.btBuscarCliente.UseVisualStyleBackColor = true;
+            // 
             // tbBuscarCpfCliente
             // 
             this.tbBuscarCpfCliente.Location = new System.Drawing.Point(116, 13);
@@ -88,9 +145,9 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.cbFuncionario);
-            this.groupBox2.Location = new System.Drawing.Point(12, 190);
+            this.groupBox2.Location = new System.Drawing.Point(344, 87);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(325, 55);
+            this.groupBox2.Size = new System.Drawing.Size(325, 88);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Funcionário";
@@ -98,60 +155,125 @@
             // cbFuncionario
             // 
             this.cbFuncionario.FormattingEnabled = true;
-            this.cbFuncionario.Location = new System.Drawing.Point(24, 19);
+            this.cbFuncionario.Location = new System.Drawing.Point(23, 35);
             this.cbFuncionario.Name = "cbFuncionario";
             this.cbFuncionario.Size = new System.Drawing.Size(281, 21);
-            this.cbFuncionario.TabIndex = 0;
+            this.cbFuncionario.TabIndex = 3;
             this.cbFuncionario.SelectedIndexChanged += new System.EventHandler(this.cbFuncionario_SelectedIndexChanged);
             // 
-            // btBuscarCliente
+            // groupBox3
             // 
-            this.btBuscarCliente.Location = new System.Drawing.Point(215, 11);
-            this.btBuscarCliente.Name = "btBuscarCliente";
-            this.btBuscarCliente.Size = new System.Drawing.Size(75, 23);
-            this.btBuscarCliente.TabIndex = 2;
-            this.btBuscarCliente.Text = "Buscar";
-            this.btBuscarCliente.UseVisualStyleBackColor = true;
+            this.groupBox3.Controls.Add(this.cbServicos);
+            this.groupBox3.Location = new System.Drawing.Point(12, 195);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(325, 74);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Serviços";
             // 
-            // label3
+            // groupBox4
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 39);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Nome:";
+            this.groupBox4.Controls.Add(this.cbProdutos);
+            this.groupBox4.Location = new System.Drawing.Point(344, 195);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(325, 74);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Produtos";
             // 
-            // label4
+            // cbServicos
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 64);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(30, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "CPF:";
+            this.cbServicos.FormattingEnabled = true;
+            this.cbServicos.Location = new System.Drawing.Point(24, 33);
+            this.cbServicos.Name = "cbServicos";
+            this.cbServicos.Size = new System.Drawing.Size(281, 21);
+            this.cbServicos.TabIndex = 4;
             // 
-            // tbNomeCliente
+            // cbProdutos
             // 
-            this.tbNomeCliente.Location = new System.Drawing.Point(65, 36);
-            this.tbNomeCliente.Name = "tbNomeCliente";
-            this.tbNomeCliente.ReadOnly = true;
-            this.tbNomeCliente.Size = new System.Drawing.Size(225, 20);
-            this.tbNomeCliente.TabIndex = 4;
+            this.cbProdutos.FormattingEnabled = true;
+            this.cbProdutos.Location = new System.Drawing.Point(23, 33);
+            this.cbProdutos.Name = "cbProdutos";
+            this.cbProdutos.Size = new System.Drawing.Size(281, 21);
+            this.cbProdutos.TabIndex = 5;
             // 
-            // tbCpfCliente
+            // dataGridView1
             // 
-            this.tbCpfCliente.Location = new System.Drawing.Point(65, 61);
-            this.tbCpfCliente.Name = "tbCpfCliente";
-            this.tbCpfCliente.ReadOnly = true;
-            this.tbCpfCliente.Size = new System.Drawing.Size(133, 20);
-            this.tbCpfCliente.TabIndex = 5;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.descricao,
+            this.valor,
+            this.remover});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 284);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(657, 180);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // descricao
+            // 
+            this.descricao.HeaderText = "Descrição";
+            this.descricao.Name = "descricao";
+            this.descricao.ReadOnly = true;
+            this.descricao.Width = 400;
+            // 
+            // valor
+            // 
+            this.valor.HeaderText = "Valor";
+            this.valor.Name = "valor";
+            this.valor.ReadOnly = true;
+            this.valor.Width = 112;
+            // 
+            // remover
+            // 
+            this.remover.HeaderText = "";
+            this.remover.Name = "remover";
+            this.remover.ReadOnly = true;
+            // 
+            // lbTotal
+            // 
+            this.lbTotal.AutoSize = true;
+            this.lbTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTotal.ForeColor = System.Drawing.Color.Firebrick;
+            this.lbTotal.Location = new System.Drawing.Point(29, 506);
+            this.lbTotal.Name = "lbTotal";
+            this.lbTotal.Size = new System.Drawing.Size(94, 37);
+            this.lbTotal.TabIndex = 6;
+            this.lbTotal.Text = "Total";
+            // 
+            // btFinalizarVenda
+            // 
+            this.btFinalizarVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btFinalizarVenda.Location = new System.Drawing.Point(517, 508);
+            this.btFinalizarVenda.Name = "btFinalizarVenda";
+            this.btFinalizarVenda.Size = new System.Drawing.Size(131, 45);
+            this.btFinalizarVenda.TabIndex = 7;
+            this.btFinalizarVenda.Text = "Finalizar Venda";
+            this.btFinalizarVenda.UseVisualStyleBackColor = true;
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancelar.Location = new System.Drawing.Point(430, 508);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(81, 45);
+            this.btCancelar.TabIndex = 8;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
             // 
             // TelaAbrirCaixa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 395);
+            this.ClientSize = new System.Drawing.Size(681, 565);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.btFinalizarVenda);
+            this.Controls.Add(this.lbTotal);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
@@ -161,6 +283,9 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -179,5 +304,16 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btBuscarCliente;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox cbServicos;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox cbProdutos;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valor;
+        private System.Windows.Forms.DataGridViewButtonColumn remover;
+        private System.Windows.Forms.Label lbTotal;
+        private System.Windows.Forms.Button btFinalizarVenda;
+        private System.Windows.Forms.Button btCancelar;
     }
 }
