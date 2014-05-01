b12f33fe2f90213250b38a1a98300f9bad1ab5ec
AndersonDrfun <andersonartur123@gmail.com>
2014-05-01 14:53:28 -0300

    Tela do Detalhar Cliente

--- Projeto/Projeto4_Junior/InterfaceGrafica/Cliente/DetalharCliente.cs
+++ Projeto/Projeto4_Junior/InterfaceGrafica/Cliente/DetalharCliente.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace Projeto4_Junior.InterfaceGrafica.Cliente
 {
     public partial class DetalharCliente : Form
@@ -16,23 +15,6 @@ namespace Projeto4_Junior.InterfaceGrafica.Cliente
         public DetalharCliente()
         {
             InitializeComponent();
-        }
-
-        public DetalharCliente(Projeto4_Junior.Modelo.Cliente cliente)
-        {
-            InitializeComponent();
-            NomCliente.Text = cliente.Nome;
-            CpfCliente.Text = cliente.Cpf;
-            TelCliente.Text = cliente.Telefone;
-            DataNascCliente.Text = cliente.DataNascimento;
-            EndeCliente.Text = cliente.Endereco;
-            EmailCliente.Text = cliente.Email;
-            
-        }
-
-        private void DetalharCliente_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
